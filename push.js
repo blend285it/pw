3fec50fe6423b9dfc34415a3bb6f7b21a970bb62
blend285it <onisuka.blend@gmail.com>
2021-02-18 12:03:43 +0700

    initial

--- push.js
+++ push.js
@@ -4,10 +4,10 @@ var push = require('web-push');
 // console.log(vapidKeys);
 
 let vapidKeys = {
-    publicKeys = 'BBzrw3bTa0peqp0AGTODqFZbvTzAVjcx31ltQxvs1kwzsU2fHICR-NgyG-7uHTlbrSZgAEiPPeCeDlSp8AhjgkE',
-    privateKeys = 'ag_iDVmHilX6vY_ziSWOFUcY2Ov28B73lY44DDPAl00'
+    publicKeys = 'BMrjpQT4KyLTl6uUBDa7_Q6V8Ra0MMj2ksZSQ5Oh2gIiQGr7N-OuGlA1-E_GsnWWECqykpeUFrGJ5TowKmkLOAY',
+    privateKeys = 'Efya0A3cYl4LDw7auzkf2Xr8SITh4s1wyVC_lgGT1tY'
 }
-push.setVapidDetails('mailto:onisu@example.com', vapidKeys.publicKeys, vapidKeys.privateKeys)
+push.setVapidDetails('mailto:onisuka.blend@gmail.com', vapidKeys.publicKeys, vapidKeys.privateKeys)
 
 let sub = {}
 
